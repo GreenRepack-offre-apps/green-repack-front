bdb82fbe7ca5492edbc52a01788b86e87ee92a03
uriel <uriel.vido@hotmail.fr>
2021-09-30 00:42:41 +0200

    suivi marchand et admin in shape

--- src/app/customers-pages/marchand/components/suivi-demande-list/suivi-demande-list.component.ts
+++ src/app/customers-pages/marchand/components/suivi-demande-list/suivi-demande-list.component.ts
@@ -13,35 +13,26 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./suivi-demande-list.component.scss']
 })
 export class SuiviDemandeListComponent implements OnInit {
-
-  items: any[] = [];// | EtatProduitData = <EtatProduitData>{};
-
   constructor(private produitService: ProduitMarchandService,
     private authService: AuthService,
     private marchandSyntheseService: MarchandSyntheseService) { }
   title_suivi_demande = 'Suivi de demande de produits';
   produitRecaps: ProduitResume[] = [];
+  validationReponse: boolean = false;
   ngOnInit(): void {
     this.authService.currentUser(new MarchandProfils()).subscribe(user => {
      if( this.authService.isFetch) {
-      //this.authService.currentUser(new MarchandProfils()).subscribe(u => {
-        // this.marchandSyntheseService.valueFilter$.next(u.email);
-      //})
-
-      // this.marchandSyntheseService.MarchandProduitStateObs.pipe(
-      //   map(dataSnapshot => {
-      //     return dataSnapshot.map((s:any) => {
-      //       const a = {key: s.key, value: s.payload.val()};
-      //       return a;
-      //     });
-      //   })
-      // ).subscribe(data => {
-      //   this.items = data })
       this.produitService.fetchMarchandProducts(user.email, null).subscribe(rst => {
         if(rst.status === 'SUCCES' && rst.data.length > 0) {
           //console.log("produits fetched: " + JSON.stringify(rst.data))
           rst.data.forEach(d => {
-            this.produitRecaps.push({recap:d, label: get_etat(d.statut_validation).label});
+            if(d.statut_validation === 'EN_ATTENTE_RECEPTION_PRODUIT') {
+              const today = new Date();
+              const nbJour = d.date_fin.getTime() - today.getTime() / (1000 * 3600 * 24);
+              this.produitRecaps.push({recap:d, label: get_etat(d.statut_validation).label, nbJourRestant: 15 - nbJour});
+            } else {
+              this.produitRecaps.push({recap:d, label: get_etat(d.statut_validation).label, nbJourRestant: 15});
+            }
           });
         }
       });
@@ -50,8 +41,27 @@ export class SuiviDemandeListComponent implements OnInit {
 
   }
 
-  label = (etat_actuelle: EtatProduitType) => {
-    EtatProduitEnum[etat_actuelle].label;
-  };
+  // evaluate = (produitRecap: ProduitResume) => {
+  //   return produitRecap?.nbJourRestant >= 0;
+  // }
+  validate(recap: ProduitRecap) {
+    switch(recap.statut_validation){
+      case 'EN_ATTENTE_REPONSE_': //passage manuelle si non automatique
+        if(this.validationReponse){
+          this.produitService.updateProduct({email_user: recap.user, idproduit: recap.idprod, etat_dem_now: recap.statut_validation,
+            etat_dem_next: 'EN_ATTENTE_RECEPTION_PRODUIT'});
+        }else{
+          this.produitService.updateProduct({email_user: recap.user, idproduit: recap.idprod, etat_dem_now: recap.statut_validation,
+            etat_dem_next: 'ANULATION'});
+        }
+        break;
+      case 'ANNULATION_EN_ATTENTE_REMBOURSEMENT':
+        this.produitService.updateProduct({email_user: recap.user, idproduit: recap.idprod, etat_dem_now: recap.statut_validation,
+          etat_dem_next: 'VALIDATION'});
+        break;
+    }
+  }
+
+
 }
 
