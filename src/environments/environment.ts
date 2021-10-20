382a67b6e6d3bb7065bffaaef054bc7350f610e8
uriel <uriel.vido@hotmail.fr>
2021-10-20 02:41:41 +0200

    automtic deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   url_backend: "http://localhost:5000",
   //url_backend: "https://green-repack.herokuapp.com",
   firebaseConfig : {
